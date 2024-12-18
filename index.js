a2d72298618a0e81b880f41165c6a0e9be3068ff
Ashvin <ashvinsunga@yahoo.com>
2024-12-18 22:29:13 +0800

    Complete but without name input validation

--- index.js
+++ index.js
@@ -16,9 +16,17 @@ const Gameboard = (() => {
             square.addEventListener('click', GameController.handleClick)
         })
     }
+    const update = (index, value) => {
+        gameboard[index] = value;
+        render();
+    }
+
+    const getGameBoard = () => gameboard;
 
     return {
         render,
+        update,
+        getGameBoard
     }
 })();
 
@@ -27,7 +35,7 @@ const Gameboard = (() => {
 const createPlayer = (name, symbol) => {
     return {
         name,
-        symbol
+        symbol,
     }
 }
 
@@ -35,7 +43,7 @@ const GameController = (() => {
     let players = [];
     // improved
     let currentPlayerIndex;
-    // improved
+    // improved 
     let isGameOver;
 
     const start = () => {
@@ -46,16 +54,85 @@ const GameController = (() => {
         currentPlayerIndex = 0;
         isGameOver = false;
         Gameboard.render();
+
     }
 
     const handleClick = (e) => {
-        console.log(e.target.id)
+        if (isGameOver){
+            return;
+        }
+        let index = parseInt(e.target.id.split("-")[1]);
+        if (Gameboard.getGameBoard()[index] !== "")
+            return;
+
+        currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
+        Gameboard.update (index, players[currentPlayerIndex].symbol);
+
+        if (winnerConfirmed(Gameboard.getGameBoard())){
+            isGameOver = true;
+            displayController.displayMessage(`${players[currentPlayerIndex].name} won the GAME!`)   
+        } else if (tieConfirmed(Gameboard.getGameBoard())){
+            isGameOver = true;
+            displayController.displayMessage(`It's tie. Please 'PLAY' again`)
+        }
+
+
+    }
+
+    const restart = () => {
+        for (let i = 0; i < 9; i++){
+            Gameboard.update(i, "")
+        }
+        document.getElementById('message').innerHTML = ""
+        isGameOver = false;
     }
     return {
         start,
-        handleClick
+        handleClick,
+        restart
     }
-})()
+})();
+
+displayController = (() => {
+    const displayMessage = (message) => {
+       document.getElementById('message').innerHTML = message
+    }
+
+    return {
+        displayMessage
+    }
+})();
+
+    function winnerConfirmed(board) {
+        const winningCombinations = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+        ]
+        for(let i = 0; i < winningCombinations.length; i++){
+            const [a, b, c] = winningCombinations[i];
+            console.log(winningCombinations[i])
+            if (board[a] && board[a] === board [b] && board[a] === board [c]){
+                return true;
+            }
+        }
+        return false
+    }
+
+    function tieConfirmed(board){
+        return board.every(cell => cell !=="")
+    }
+
+
+const restartButton = document.getElementById('restart-button')
+restartButton.addEventListener('click', () => {
+    GameController.restart()
+})
 
 const startButton = document.getElementById('start-button')
 startButton.addEventListener('click',  () => {
